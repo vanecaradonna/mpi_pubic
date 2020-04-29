4b5770879d6482bbd177340e616729a3c379405f
vanecaradonna <60409750+vanecaradonna@users.noreply.github.com>
2020-04-29 12:47:06 -0300

    Update ejercicio1_c.c

--- ejercicio1_c.c
+++ ejercicio1_c.c
@@ -13,11 +13,13 @@ int main (int argc, char **argv) {
     // Recepcion de los mensajes.
     MPI_Gather(&proceso, 1, MPI_INT, buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
     if (proceso==0){
-        printf("Hola, soy el proceso 0 (hay %d procesos) y recibo: %d \n", &total_pro);
-        for (int i = 0; i < total_pro - 1;i++) {
+        printf("Hola, soy el proceso 0 (hay %d procesos) y recibo: \n", total_pro);
+        int i = 0; 
+	while(i < total_pro ) {
             printf("Hola desde el proceso %d \n", buffer[i]);
+	    i++;
         }
-	}
+    }
     MPI_Finalize();
     return(0);
 }
