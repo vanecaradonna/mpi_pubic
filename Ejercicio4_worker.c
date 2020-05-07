4dbbfbd3a86d82da7ec09908d95120e450e847bc
vanecaradonna <vanejofrecaradonna@gmail.com>
2020-05-07 13:11:54 -0300

    a

---
+++ Ejercicio4_worker.c
@@ -0,0 +1,11 @@
+/* worker */
+#include <mpi.h>
+int main(int argc, char* argv[]) {
+	MPI_Init(&argc, &argv);
+	MPI_Comm_get_parent(&parent);
+	if (parent == MPI_COMM_NULL) error("No parent!");
+	MPI_Comm_remote_size(parent, &size);
+	if (size != 1) error("Something's wrong with the parent");
+	MPI_Finalize();
+	return 0;
+}
