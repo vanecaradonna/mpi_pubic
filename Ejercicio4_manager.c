d0c95055fae3f5094a8ecb2bee5a12dc2fbf2c2e
vanecaradonna <vanejofrecaradonna@gmail.com>
2020-05-07 13:19:41 -0300

    4

--- Ejercicio4_manager.c
+++ Ejercicio4_manager.c
@@ -4,16 +4,19 @@ int main(int argc, char* argv[]) {
 	MPI_Comm everyone; /* intercommunicator */
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	if (world_size != 1) error("Top heavy with management");
+	if (world_size != 1) 
+		error("Top heavy with management");
 	MPI_Comm_get_attr(MPI_COMM_WORLD, MPI_UNIVERSE_SIZE, &universe_sizep, &flag);
 	if (!flag) {
 		printf("This MPI does not support UNIVERSE_SIZE. How many\n\processes total?");
 		scanf("%d", &universe_size);
 	}
-	else universe_size = *universe_sizep;
-	if (universe_size == 1) error("No room to start workers");
+	else 
+		universe_size = *universe_sizep;
+	if (universe_size == 1) 
+		error("No room to start workers");
 	choose_worker_program(worker_program); /* Hace el spawn */
-	MPI_Comm_spawn(worker_program, MPI_ARGV_NULL, universe_size - 1, MPI_INFO_NULL, 0,
-		MPI_COMM_SELF, &everyone, MPI_ERRCODES_IGNORE);
-	MPI_Finalize(); return 0;
+	MPI_Comm_spawn(worker_program, MPI_ARGV_NULL, universe_size - 1, MPI_INFO_NULL, 0, MPI_COMM_SELF, &everyone, MPI_ERRCODES_IGNORE);
+	MPI_Finalize();
+	return 0;
 }
--- Ejercicio4_worker.c
+++ Ejercicio4_worker.c
@@ -3,9 +3,11 @@
 int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_get_parent(&parent);
-	if (parent == MPI_COMM_NULL) error("No parent!");
+	if (parent == MPI_COMM_NULL)
+		error("No parent!");
 	MPI_Comm_remote_size(parent, &size);
-	if (size != 1) error("Something's wrong with the parent");
+	if (size != 1)
+		error("Something's wrong with the parent");
 	MPI_Finalize();
 	return 0;
 }
