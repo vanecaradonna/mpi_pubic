0dbd5db187cc0adf03440e6c810d912b41c701a9
vanecaradonna <vanejofrecaradonna@gmail.com>
2020-05-08 12:02:19 -0300

    Update Ejercicio4_manager.c

--- Ejercicio4_manager.c
+++ Ejercicio4_manager.c
@@ -2,22 +2,27 @@
 /* manager */
 #include <mpi.h>
 int main(int argc, char** argv){
-	int world_size, universe_size, * universe_sizep, flag;
+	int world_size, universe_size, *universe_sizep, flag;
 	MPI_Comm everyone; /* intercommunicator */
 	char worker_program[100];
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	if (world_size != 1)
-		error("Top heavy with management");
+		printf("Top heavy with management");
 	MPI_Comm_get_attr(MPI_COMM_WORLD, MPI_UNIVERSE_SIZE,&universe_sizep, &flag);
 	if (flag == 0)  {
-		printf("This MPI does not support UNIVERSE_SIZE\n How many processes total ? \n");
+		printf("MPI no soporta universe_size\n");
+		printf("cuantos procesos totales? \n");
+		//printf("This MPI does not support UNIVERSE_SIZE \n How many processes total ? \n");
 		scanf("%d", &universe_size);
 	}
-	else
+	else {
 		universe_size = *universe_sizep;
-	if (universe_size == 1)
-		error("No room to start workers");
+	}
+	if (universe_size == 1){
+		printf("No room to start workers");
+	}
+	
 	/*
 	* Now spawn the workers. Note that there is a runtime
 	* determination of what type of worker to spawn, and presumably
